da4f08b9c3867d8845c0cd82874894458fdbcb88
Kinza Ahmed <kinza1013@yahoo.com>
2022-02-13 17:43:08 -0600

    Started survey

--- app/views.py
+++ app/views.py
@@ -1,9 +1,26 @@
 from app import app
+from flask import render_template
 
 @app.route("/")
 def index():
-    return "Hello world"
+    return render_template("home.html")
 
 @app.route("/survey")
-def about():
-    return "Survey time!"
+def survey():
+    return render_template("survey.html")
+
+@app.route("/decline")
+def decline():
+    return render_template("decline.html")
+
+@app.route("/thanks")
+def thanks():
+    return render_template("thanks.html")
+
+@app.route("/api/results")
+def results():
+    return render_template("results.html")
+
+@app.route("/admin/summary")
+def summary():
+    return render_template("summary.html")
